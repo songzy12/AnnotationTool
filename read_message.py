898dd8ecb0815af22af6bbc834581abe27777fca
songzy <songzy.thu@qq.com>
2018-11-18 17:25:35 +0800

    huanyihuan answer fix

--- read_message.py
+++ read_message.py
@@ -10,14 +10,15 @@ message = xiaomu.message
 
 def get_messages(course_id):
     message_set = message.find(
-        {'course_id': course_id, 'flag': {"$in": [None, 'more', 'try', "cached", "cached_more"]}, 'question_source': None}).sort('_id', -1)
+        {'course_id': course_id, 'flag': {"$in": [None, 'more', 'try', "cached", "cached_more"]}}).sort('_id', -1)
 
     q_dict, a_list = {}, []
     for m in message_set:
         if 'message' not in m.keys():
             continue
 
-        if m['type'] == 'question':
+        if m['type'] == 'question' and m.get('question_source', None) not in ['wobudong', 'active_question']:
+
             q_dict[m['_id']] = m
 
         if m['type'] == 'answer':
@@ -30,7 +31,7 @@ def get_messages(course_id):
 
     qid_list, q_text, a_text, times, tags = [], [], [], [], []
     for v in a_list:
-        if 'origin_question' not in v.keys():
+        if 'origin_question' not in v:
             continue
 
         q_id = v['origin_question']
@@ -42,7 +43,8 @@ def get_messages(course_id):
 
         qid_list.append(q_id)
         q_text.append(q_dict[q_id]['message'])
-        a_text.append(v['message'])
+        a_text.append(v['message'] if v['message'] else (v['answers'][0]['message'] if 'answers' in v else ''))
+        
         times.append(v['time'])
         tags.append(v.get('tag', -1))
 
